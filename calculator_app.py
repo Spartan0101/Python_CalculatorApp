ed67f6bce546fa9ddb7d2c490a04c7d6d05bc71e
Spartan1010 <tranulovianos@gmail.com>
2024-06-01 08:12:09 +0100

    Calculator App - Functions with outputs

--- calculator_app.py
+++ calculator_app.py
@@ -20,22 +20,35 @@ operations = {
 }
 #function = operation["*"]
 #function(2, 3)
-num1 = int(input("What's the first number?: "))
+def calculator():
+    num1 = float(input("What's the first number?: "))
 
-for symbol in operations:
-    print(symbol)
-num2 = int(input("What's the second number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-operation_symbol = input("Pick an operation from the line above: ")
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+    while should_continue:
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-answer = calculation_function(answer, num3)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+            #print("Goodbye")
+calculator()
+#operation_symbol = input("Pick another operation: ")
+#num3 = int(input("What's the next number?: "))
+#calculation_function = operations[operation_symbol]
+#second_answer = calculation_function(first_answer, num3)
+
+#print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
 
 # Days in month
 '''
