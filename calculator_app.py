b33ff269bbf3e411d1e80c662d8945fe27da1475
Spartan1010 <tranulovianos@gmail.com>
2024-06-01 07:49:46 +0100

    Calculator App - Functions with outputs

--- calculator_app.py
+++ calculator_app.py
@@ -1,1 +1,78 @@
-print("CalculatorApp")
+#Functions with Outputs
+
+#The Calculator App
+
+
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def multiply(n1, n2):
+    return n1 * n2
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+#function = operation["*"]
+#function(2, 3)
+num1 = int(input("What's the first number?: "))
+
+for symbol in operations:
+    print(symbol)
+num2 = int(input("What's the second number?: "))
+
+operation_symbol = input("Pick an operation from the line above: ")
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("What's the next number?: "))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(answer, num3)
+
+# Days in month
+'''
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+# TODO: Add more code here 👇
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if month == 2 and is_leap(year):
+        return 29
+    else:
+        return month_days[month - 1]
+
+# 🚨 Do NOT change any of the code below
+year = int(input())  # Enter a year
+month = int(input())  # Enter a month
+days = days_in_month(year, month)
+print(days)
+'''
+
+'''
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    #print(f"{formated_f_name} {formated_l_name}")
+    return f"{formated_f_name} {formated_l_name}"
+
+formated_string = format_name("ianos", "SZAR")
+print(formated_string)
+'''
